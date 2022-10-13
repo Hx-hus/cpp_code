a7218fcb8d0b3a3a452809f83b885f0f3e5b1e3f
xiao-zhangs-code-road <3203423831@qq.com>
2022-10-13 23:01:57 +0800

    c

---
+++ 13_10_22/13_10_22/1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 40;
+	int c = 212;
+	int b = (-8 + 22)*a - 10 + c/2;
+	printf("%d\n", b);
+
+
+	return 0;
+}
+
+
+
+//int main()
+//{
+//	printf("I lost my cellphone!\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	scanf_s("%d\n", &a);
+//	if (a >= 140)
+//		printf("Genius\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//
+//	return 0;
+//}
