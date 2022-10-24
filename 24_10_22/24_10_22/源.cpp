e046dec75b3222cab70c0110d83447b1036f1125
xiao-zhangs-code-road <3203423831@qq.com>
2022-10-24 23:31:50 +0800

    c

---
+++ 24_10_22/24_10_22/源.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 0; 
+//	int b = 0;
+//	int c = a * b;
+//	for (a = 1; a <= 9; a++)
+//	{
+//		for (b = 1; b <= 9; b++)
+//		{
+//			c = a * b;
+//			printf("%d*%d=%d ", a, b, c);
+//		}
+//	}
+//	
+//	return 0;
+//}
