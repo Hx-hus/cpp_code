88d26a73c821cabb663b597b217444d862af5333
xiao-zhangs-code-road <3203423831@qq.com>
2022-12-25 21:06:34 +0800

    c

---
+++ 25_12_22/25_12_22/1.C
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int option = 0;
+//	printf("今天是工作日还是休息日？\n");
+//	scanf("%d", &option);
+//	if (option == 1)
+//	{
+//		printf("工作日");
+//	}
+//	else if (option == 2)
+//	{
+//		printf("工作日");
+//	}	
+//	else if (option == 3)
+//	{
+//		printf("工作日");
+//	}	
+//	else if (option == 4)
+//	{
+//		printf("工作日");
+//	}	
+//	else if (option == 5)
+//	{
+//		printf("工作日");
+//	}	
+//	else if (option == 6)
+//	{
+//		printf("休息日");
+//	}	
+//	else if (option == 7)
+//	{
+//		printf("休息日");
+//	}
+//	else
+//	{
+//		printf("选择错误");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int option = 0;
+//	printf("你会去努力敲代码吗？（选择1（是） or 2（不是））\n");
+//	scanf("%d", &option);
+//	if (option == 1)
+//	{
+//		printf("恭喜你，好offer在前面等着你\n");
+//	}
+//	else if(option==2)
+//	{
+//		printf("不好意思，进厂吧\n");
+//	}
+//	else
+//	{
+//		printf("选择错误\n");
+//	}
+//	return 0;
+//}
+int main()
+{
+	int option = 0 ;
+	//用于提示选择
+	printf("你会去努力敲代码吗？（选择1（是） or 2（不是））\n");
+	scanf("%d", &option);
+	if (option == 1)
+	{
+		printf("恭喜你，好offer在前面等着你\n");
+	}
+	else
+	{
+		printf("不好意思，进厂吧\n");
+	}
+	return 0;
+}
+
+//if (满足什么条件进入)
+//{
+//	；
+//}
+//else
+//{
+//	;
+//}
