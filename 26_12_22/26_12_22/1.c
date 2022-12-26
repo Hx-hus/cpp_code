f805c95a106e40d9273a82a2535c82850135a864
xiao-zhangs-code-road <3203423831@qq.com>
2022-12-26 21:55:00 +0800

    c

---
+++ 26_12_22/26_12_22/1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include<stdio.h>
+
+int main()
+{
+	int count = 0;
+	while (count < 10000)
+	{
+		printf("学习新知识第%d天\n",count);
+		count++;
+	}
+	printf("拿到好offer");
+	return 0;
+}
