47a747996de780ad70e04c374b9e0e03b6270a2d
Hx-hus <3203423831@qq.com>
2023-04-12 22:36:33 +0800

    test.cpp

---
+++ 12_4_23/12_4_23/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include<iostream>
+
+using namespace std;
+
+using std::cout;
+using std::endl;
+
+namespace jiale
+{
+	int a = 1;
+}
+int main()
+{
+	std::cout << "hello C++" << std :: endl;
+	return 0;
+}
