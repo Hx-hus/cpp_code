0f36074eb6bd8d2d58bbd0c0a4292ea6fdc959aa
Hx-hus <3203423831@qq.com>
2023-04-13 22:38:43 +0800

    刷题

---
+++ 13_4_23/13_4_23/test.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include<iostream>
+#include<algorithm>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
+	std::cout << "Ë®Ò»²¨gitee" << std::endl;
+	std::cout << "Ãî" << std::endl;
+	return 0;
+}
