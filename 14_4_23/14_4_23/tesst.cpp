d065e16bf11d77cdd05d8e5b85bfadff4b668b65
Hx-hus <3203423831@qq.com>
2023-04-14 22:18:14 +0800

    ikun

---
+++ 14_4_23/14_4_23/tesst.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include<iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+#define 再多一眼看一眼就会爆炸 int main(){
+#define 再近一点靠近点快被融化 std::cout << "ikun" << std::endl
+#define 想把你占位己有 return 0
+#define baby }
+
+再多一眼看一眼就会爆炸
+再近一点靠近点快被融化;
+想把你占位己有;
+baby
